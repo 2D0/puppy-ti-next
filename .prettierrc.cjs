5f5c290c47ddacb527c6ebab0a392d80fe21ccd7
2D0 <lo7733ve@naver.com>
2023-07-22 17:37:09 +0900

    [Docs] 탭 2개로 변경

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     //코드 마지막에 세미콜론이 자동 생성
     useTabs: false,
     //탭의 사용을 금하고 스페이스바 사용으로 대체
-    tabWidth: 4,
+    tabWidth: 2,
     // 들여쓰기 너비는 4칸
     trailingComma: 'all',
     // 객체나 배열 키:값 뒤에 콤마 생성
