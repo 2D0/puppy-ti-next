94780cdc94f6faf8c870296527e1059a784cee9b
2D0 <70118192+2D0@users.noreply.github.com>
2023-08-16 13:09:22 +0900

    [Refactor] Refactor

--- src/components/molecules/Share/Share.jsx
+++ src/components/molecules/Share/Share.jsx
@@ -4,12 +4,17 @@ import * as A from '@/components/molecules/Share/Share.style';
 
 //이미지
 import SvgComponent from '@/assets/img/Icons/SvgComponent';
+import { ShareIcon } from '@/api/Icons/ShareIcon';
 
 const Share = () => {
   const snsImg = ['FACEBOOK', 'KAKAO', 'TWITTER', 'LINK'];
   return (
     <>
       <A.ShareWrap>
+        {ShareIcon.map(({ name, icon }) => {
+          return <A.ShareButtonAtom href="/">{icon()}</A.ShareButtonAtom>;
+        })}
+
         {snsImg.map(item => {
           return (
             <A.ShareButtonAtom href="/">
