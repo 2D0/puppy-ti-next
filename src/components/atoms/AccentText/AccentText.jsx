18fac28688a891f6c3b0efb2dd4477ec00065cc3
2D0 <lo7733ve@naver.com>
2023-09-13 22:10:20 +0900

    [Refactor] 컴포넌트 명 변경

--- src/components/atoms/AccentText/AccentText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-//스타일
-import * as A from '@/components/atoms/AccentText/AccentTxet.style';
-
-const AccentText = ({ shape, data }) => {
-  return <A.AccentTextAtom>{data}</A.AccentTextAtom>;
-};
-export default AccentText;
--- src/components/atoms/DefaultText/DefaultText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-//스타일
-import { DefaultTextAtom } from '@/components/atoms/DefaultText/DefaultText.style';
-
-const DefaultText = ({ data }) => {
-  return <DefaultTextAtom>{data}</DefaultTextAtom>;
-};
-export default DefaultText;
