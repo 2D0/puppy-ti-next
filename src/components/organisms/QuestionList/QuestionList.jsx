23ea2a271e54571174bbbee922d92eab15291382
2D0 <lo7733ve@naver.com>
2023-09-16 01:33:01 +0900

    [Refactor] 체크 페이지 작업 중, axios로 질문 데이터 받아오기

--- src/components/organisms/QuestionList/QuestionList.jsx
+++ src/components/organisms/QuestionList/QuestionList.jsx
@@ -1,39 +1,15 @@
 import React from 'react';
-import questionData from '@/app/api/Check/questions.json';
 
 //스타일
-import * as O from '@/components/organisms/QuestionList/QuestionList.style';
+import { CheckQABoxList, CheckQABoxs } from '@/components/organisms/QuestionList/QuestionList.style';
 
-const QuestionList = () => {
+const QuestionList = questionData => {
   return (
-    <O.CheckQABoxs>
-      {question.map((item, itemIdx) => (
-        <O.CheckQABoxList key={item.id}>
-          <O.CheckQATop>
-            <O.CheckQATitL>아니다</O.CheckQATitL>
-            <O.CheckQATitR>그렇다</O.CheckQATitR>
-          </O.CheckQATop>
-          <O.CheckQACont>
-            <O.CheckQATxt>{item.text}</O.CheckQATxt>
-            <O.CheckQABtns>
-              {questionData.scoreType.map((scoreItem, scoreIdx) => (
-                <O.CheckQABtn
-                  key={scoreItem.id}
-                  onClick={() => {
-                    checkState(item.id, scoreItem.id);
-                  }}
-                  type="button"
-                >
-                  <O.CheckFoot questionItem={item} questionIdx={questionIdx} score={score} scoreIdx={scoreIdx} scoreCount={scoreItem.score}>
-                    <Foot />
-                  </O.CheckFoot>
-                </O.CheckQABtn>
-              ))}
-            </O.CheckQABtns>
-          </O.CheckQACont>
-        </O.CheckQABoxList>
+    <CheckQABoxs>
+      {questionData.questionData.map(item => (
+        <CheckQABoxList key={item.id}>{item.text}</CheckQABoxList>
       ))}
-    </O.CheckQABoxs>
+    </CheckQABoxs>
   );
 };
 export default QuestionList;
--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,3 +1,3 @@
 export { default as Header } from './Header/Header.jsx';
 export { default as Footer } from './Footer/Footer.jsx';
-// export { default as QuestionList } from './QuestionList/QuestionList.jsx';
+export { default as QuestionList } from './QuestionList/QuestionList.jsx';
