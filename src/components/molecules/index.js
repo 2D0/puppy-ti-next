0d64a6e224e93a0bdf9417bc8f2d365db0976c8e
2D0 <lo7733ve@naver.com>
2023-09-15 23:48:02 +0900

    [chore] 필요 없는 코드 삭제

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,7 +1,2 @@
-export { default as CheckHead } from './CheckHead/CheckHead.jsx';
-export { default as PageShare } from './PageShare/PageShare.jsx';
-export { default as PercentBar } from './PercentBar/PercentBar.jsx';
-export { default as Question } from './Question/Question.jsx';
 export { default as Share } from './Share/Share.jsx';
 export { default as TitleBox } from './TitleBox/TitleBox.jsx';
-export { default as RowText } from './RowText/RowText.jsx';
