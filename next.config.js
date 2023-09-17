51354ce95c92f09afd16b293bcda1295ce607e97
2D0 <lo7733ve@naver.com>
2023-09-18 00:39:00 +0900

    [Docs] 이미지 로드시 avif 로 변환 해서 출력 하고 avif 안 나오는 브라우저는 webp로 출력되게

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  // images: {
-  //   formats: ['image/avif', 'image/webp'],
-  // },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
   async rewrites() {
     return [
       {
@@ -18,26 +18,26 @@ const nextConfig = {
     ];
   },
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
+    // SVG 가져오기를 처리하는 기존 규칙을 가져옵니다.
     const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
+      // 기존 규칙을 다시 적용합니다. 단, ?url로 끝나는 svg 가져오기에만 해당됩니다.
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
         resourceQuery: /url/, // *.svg?url
       },
-      // Convert all other *.svg imports to React components
+      // 다른 모든 *.svg 가져오기를 React 구성 요소로 변환
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ['@svgr/webpack', { loader: 'file-loader' }, { loader: 'svgo-loader' }, { loader: 'xml-fix-loader' }],
+        use: ['@svgr/webpack', { loader: 'file-loader' }],
       },
     );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    // *.svg를 무시하도록 파일 로더 규칙을 수정합니다. 이제 처리가 완료되었기 때문입니다.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
