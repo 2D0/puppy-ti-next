45387505d7dc3b3c7b704115cd0159846ad69e9d
2D0 <lo7733ve@naver.com>
2023-09-19 00:06:41 +0900

    [Delete] 필요 없는 파일 삭제

--- src/components/atoms/ImgIcon/ImgIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-//이미지
-import { SnsKakao } from '@/assets/img/Icons';
-
-const ImgIcon = () => {
-  return <></>;
-};
-export default ImgIcon;
--- src/components/atoms/Logo/logo.jsx
+++
@@ -1,3 +0,0 @@
-import Logo from '@/assets/img/Logo/logo.svg';
-//
-// <Logo></Logo>
