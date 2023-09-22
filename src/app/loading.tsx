0e2d810ff8651598567ad94e8ff7bfe3d4e4bf56
2D0 <lo7733ve@naver.com>
2023-09-22 19:19:12 +0900

    [Feat] 로딩 페이지 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,46 @@
+'use client';
+import React from 'react';
+
+import { ReactComponent as Toe1 } from '@/assets/img/Icons/ico-finger1.svg';
+import { ReactComponent as Toe2 } from '@/assets/img/Icons/ico-finger2.svg';
+import { ReactComponent as Toe3 } from '@/assets/img/Icons/ico-finger3.svg';
+import { ReactComponent as Toe4 } from '@/assets/img/Icons/ico-finger4.svg';
+import { ReactComponent as ToeBody } from '@/assets/img/Icons/ico-footbody.svg';
+import { TitleBox } from '@/components/molecules';
+import { LightPurple } from '@/style/Common.style';
+import { LoadingWrap, ToeComponent1, ToeComponent2, ToeComponent3, ToeComponent4, ToeComponentBody, ToesWrap } from '@/style/loading.style';
+
+const loadingTextData = {
+  accent: {
+    text: 'LOADING',
+    color: LightPurple,
+    font: true,
+  },
+};
+const Loading = () => {
+  return (
+    <>
+      <LoadingWrap>
+        <ToesWrap>
+          <ToeComponent1>
+            <Toe1 />
+          </ToeComponent1>
+          <ToeComponent2>
+            <Toe2 />
+          </ToeComponent2>
+          <ToeComponent3>
+            <Toe3 />
+          </ToeComponent3>
+          <ToeComponent4>
+            <Toe4 />
+          </ToeComponent4>
+          <ToeComponentBody>
+            <ToeBody />
+          </ToeComponentBody>
+        </ToesWrap>
+        <TitleBox shape={loadingTextData} />
+      </LoadingWrap>
+    </>
+  );
+};
+export default Loading;
